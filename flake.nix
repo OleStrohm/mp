379a8a4f76ffbf3073f1f2f4badc6aa2e05d70dc
Ole Strohm <strohm99@gmail.com>
2024-02-28 23:14:54 +0000

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [ rustc cargo pkg-config ];
+          buildInputs = with pkgs; [
+            udev alsa-lib vulkan-loader
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
+            libxkbcommon wayland
+          ];
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
